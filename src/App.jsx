a259671a8895236588b9b71494a1db29a7c5c942
Barrett Anderson <barrett1614@gmail.com>
2024-02-05 20:21:24 -0600

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <UserInput />
+      {/* Results here */}
+    </>
+  );
+}
+
+export default App;
